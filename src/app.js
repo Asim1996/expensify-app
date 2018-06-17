ef4ffb06eb6ff807a34a7da7cbcd3ba36a0e70d4
Asim1996 <awesomeasim2596@gmail.com>
2018-06-17 17:19:29 +0530

    Setup formatting for amount and created at

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ import './styles/styles.scss';
 
 const store=configureStore();
 store.dispatch(addExpense({description:'Water bill',amount:4500}))
-// console.log(store.getState());
+console.log(store.getState());
 store.dispatch(addExpense({description:'Gas bill',createdAt:1000}))
 store.dispatch(addExpense({description:'Rent',amount:109500}))
 
