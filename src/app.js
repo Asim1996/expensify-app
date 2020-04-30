7f3bdddad3aa5b223da6d3f9e9b0f3a1e9ff16ad
Asim Ansari <asim@Asims-MacBook-Air.local>
2020-04-30 20:29:39 +0530

    add private route

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import AppRouter from './routers/AppRouter'
+import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './stores/configureStore';
 import { startSetExpense } from './actions/expenses';
+import { login, logout } from './actions/auth';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 // require('dotenv').config()
 const store = configureStore();
 const jsx = (
@@ -16,11 +18,33 @@ const jsx = (
 		<AppRouter />
 	</Provider>
 );
+let hasRendered = false;
+const renderApp = () => {
+	if (!hasRendered) {
+		ReactDOM.render(jsx, document.getElementById('app'));
+		hasRendered = true;
+	}
+}
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpense()).then(() => {
-	ReactDOM.render(jsx, document.getElementById('app'));
+
+firebase.auth().onAuthStateChanged((user) => {
+	if (user) {
+		store.dispatch(login(user.uid))
+		store.dispatch(startSetExpense()).then(() => {
+			if(history.location.pathname == "/"){
+				history.push('/dashboard');
+			}
+			renderApp();
+		})
+
+	} else {
+		// console.log("log out");
+		store.dispatch(logout());
+		renderApp();
+		history.push('/');
+	}
 })
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-
-const Header = () => (
-	<header>
-	<h1>Expensify App</h1>
-	<NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>		
-	<NavLink to="/create" activeClassName="is-active">Create</NavLink>		
-	<NavLink to="/edit/:id" activeClassName="is-active">Edit</NavLink>
-	<NavLink to="/help" activeClassName="is-active">Help</NavLink>
-	</header>
-);
-export default Header;
+import {connect} from 'react-redux';
+import {startLogout} from '../actions/auth'
+const Header = (props) => {
+	console.log(props);
+	return(
+		<header>
+		<h1>Expensify App</h1>
+		<NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>		
+		<NavLink to="/create" activeClassName="is-active">Create</NavLink>		
+		<button onClick = {()=>{props.dispatch(startLogout())}}>Logout</button>
+		{/* <NavLink to="/edit/:id" activeClassName="is-active">Edit</NavLink>
+		<NavLink to="/help" activeClassName="is-active">Help</NavLink> */}
+		</header>
+	)
+}
+export default connect()(Header);
