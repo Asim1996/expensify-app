ab0343cdd92539eaca3e2a7adf275fa1a116afc0
Asim1996 <awesomeasim2596@gmail.com>
2020-05-04 23:07:52 +0530

    add css to login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {startLogout} from '../actions/auth'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth'
 const Header = (props) => {
-	console.log(props);
-	return(
-		<header>
-		<h1>Expensify App</h1>
-		<NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>		
-		<NavLink to="/create" activeClassName="is-active">Create</NavLink>		
-		<button onClick = {()=>{props.dispatch(startLogout())}}>Logout</button>
-		{/* <NavLink to="/edit/:id" activeClassName="is-active">Edit</NavLink>
-		<NavLink to="/help" activeClassName="is-active">Help</NavLink> */}
+	return (
+		<header className="header">
+			<div className="content-container">
+				<div className="header__content">
+					<Link to="/" exact={true} className="header__title">
+						<h1>Expensify App</h1>
+					</Link>
+					<button onClick={() => { props.dispatch(startLogout()) }}>Logout</button>
+				</div>
+			</div>
 		</header>
 	)
 }
