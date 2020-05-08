2833720a0a5d7adc353e34d66b005f7f0f602b31
Asim Ansari <awesomeasim2596@gmail.com>
2020-05-09 00:48:24 +0530

    add loader

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from "../images/loader.gif" ;
+const Loadingpage = () => (
+    <div className="loader">
+        <img className="loader__image" src="/images/loader.gif" alt="Loading.." />
+    </div>
+
+)
+
+export default Loadingpage;
